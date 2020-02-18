3712bda1d7bc885bde98ae3bbcd2ec2846605134
KryptoCrash <kryptocrashmh@gmail.com>
2020-02-18 11:33:11 -0500

    added formated dataset functions and image to tensor. NEED TO ADD RELATIVE CENTERS

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import tensorflow.keras.layers as layers
+from tensorflow import keras
+from keras import backend as k
 import IPython.display as display
 from PIL import Image, ImageDraw
 import numpy as np
@@ -13,9 +15,12 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 BATCH_SIZE = 32
 IMG_HEIGHT = 416
 IMG_WIDTH = 416
+GRID_CELLS = 12
+N_BOXES = 2
+N_CLASSES = 1
 
 
-def build(img_w, img_h, grid_w, grid_h, nb_boxes, nb_classes):
+def build(img_w, img_h, grid_w, grid_h, n_boxes, n_classes):
     inputs = tf.keras.Input(shape=(img_w, img_h, 3))
     x = layers.Conv2D(16, (1, 1))(inputs)
     x = layers.Conv2D(32, (3, 3))(x)
@@ -28,9 +33,9 @@ def build(img_w, img_h, grid_w, grid_h, nb_boxes, nb_classes):
     x = layers.Flatten()(x)
     x = layers.Dense(256, activation='sigmoid')(x)
     x = layers.Dense(
-        grid_w * grid_h * (nb_boxes * 5 + nb_classes), activation='sigmoid')(x)
+        grid_w * grid_h * (n_boxes * 5 + n_classes), activation='sigmoid')(x)
     outputs = layers.Reshape(
-        (grid_w * grid_h, (nb_boxes * 5 + nb_classes)))(x)
+        (grid_w * grid_h, (n_boxes * 5 + n_classes)))(x)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs, name='YoloV3')
 
@@ -52,7 +57,6 @@ def _parse_image_function(example_proto):
     return tf.io.parse_single_example(example_proto, image_feature_description)
 
 
-
 parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
 parsed_image_dataset = parsed_image_dataset.shuffle(500)
 
@@ -65,12 +69,14 @@ def decode_img(img):
     # resize the image to the desired size.
     return tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
 
+
 def encode_img(tensor):
     # resize the image to the desired size.
     # Use `convert_image_dtype` to convert to uint8.
     img = tf.image.convert_image_dtype(tensor, tf.uint8)
     # convert the 3D uint8 tensor to a compressed string
     return tf.image.encode_jpeg(img)
+
 
 def process_path(file_path):
     # load the raw data from the file as a string
@@ -79,26 +85,57 @@ def process_path(file_path):
     return img
 
 
-# Testing Code to Draw the Bounding Boxes
-for image_features in parsed_image_dataset.take(1):
-    filename = image_features['image/filename'].numpy().decode('utf-8')
-    print(filename)
-    tensor = process_path('./images/power_cell/' + filename)
-    fh = open("imageToSave.jpeg", "wb")
-    fh.write(encode_img(tensor).numpy())
-    fh.close()
-    pic = Image.open('imageToSave.jpeg')
-    draw = ImageDraw.Draw(pic)
-    xmins = image_features['image/object/bbox/xmin'].values.numpy()
-    xmaxes = image_features['image/object/bbox/xmax'].values.numpy()
-    ymins = image_features['image/object/bbox/ymin'].values.numpy()
-    ymaxes = image_features['image/object/bbox/ymax'].values.numpy()
+def format_dataset(dataset):
+    images = []
+    labels = []
+    for element in dataset:
+        # Map elements through function to turn them into tuples with format (image, label)
+        filename = element['image/filename'].numpy().decode('utf-8')
+        image_tensor = process_path('./images/power_cell/' + filename)
+        # Map labels to format [gridPos, [x, y, w, h, conf]]
+        label_tensor = k.zeros([GRID_CELLS ** 2, 2 * N_BOXES + N_CLASSES])
+        x_mins = element['image/object/bbox/xmin'].values.numpy()
+        x_maxes = element['image/object/bbox/xmax'].values.numpy()
+        y_mins = element['image/object/bbox/ymin'].values.numpy()
+        y_maxes = element['image/object/bbox/ymax'].values.numpy()
+        widths = x_maxes - x_mins
+        heights = y_maxes - y_mins
+        x_centers = x_mins + widths / 2
+        y_centers = y_mins + heights / 2
+        grid_x = tf.math.floor(x_centers * 12)
+        grid_y = tf.math.floor(y_centers * 12)
+        for i in range(grid_x.shape[0]):
+            label_tensor[int(grid_y[i] * GRID_CELLS + grid_x[i])].assign(
+                [x_centers[i], y_centers[i], widths[i], heights[i], 1])
+        images.append(image_tensor)
+        labels.append(label_tensor)
+    return images, labels
 
-    for i in range(xmins.size):
-        print(i)
-        draw.rectangle([(xmins[i] * 416, ymins[i] * 416), (xmaxes[i] * 416, ymaxes[i] * 416)], outline=0xff0000,
+
+images, labels = format_dataset(parsed_image_dataset)
+
+fh = open("imageToSave.jpeg", "wb")
+fh.write(encode_img(images[0]).numpy())
+fh.close()
+pic = Image.open('imageToSave.jpeg')
+draw = ImageDraw.Draw(pic)
+example_label = labels[0]
+label = example_label[..., 4] > 0.3
+for i in range(label.shape[0]):
+    if label[i]:
+        x_center = example_label[i][0]
+        y_center = example_label[i][1]
+        width = example_label[i][2]
+        height = example_label[i][3]
+        x_min = x_center - width / 2
+        y_min = y_center - height / 2
+        x_max = x_min + width
+        y_max = y_min + height
+        draw.rectangle([(x_min * IMG_WIDTH, y_min * IMG_HEIGHT), (x_max * IMG_WIDTH, y_max * IMG_HEIGHT)],
+                       outline=0xff0000,
                        width=3, fill=None)
-    pic.show()
+pic.show()
+
 
 # I'm pretty sure this is the dataset now
 # train_ds=prepare_for_training(labeled_ds)
